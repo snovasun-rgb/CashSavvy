cce6a7533e1270b8ddd6f12e5234bab3c3c33dbd
snovasun-rgb <snovasun@gmail.com>
2025-08-21 00:42:08 +0530

    Fix JSX extension error

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FinMatePrototype from '../fintech'
+import FinMatePrototype from '../fintech.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
